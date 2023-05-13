4521eb990e38095635f635aee0ccc6996bcb0669
Neykox <anthonyleroy2017@gmail.com>
2023-05-13 19:44:47 +0200

    need commit to checkout

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { TwoFaModule } from './two_fa/two_fa.module';
 
 @Module({
-    imports: [AuthModule],
+    imports: [AuthModule, TwoFaModule],
 })
 export class AppModule {}
