a918dfc90c98c63ee815b4b16799a62cc6f39a3d
RoyaLProg <royal@RoyalPC>
2023-07-06 08:26:15 +0200

    UX : starting to have something for navbar

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -4,34 +4,37 @@ import messageImg from '../../asset/images/message.png';
 import userImg from '../../asset/images/user.png';
 import crowdImg from '../../asset/images/crowd.png';
 import settings from '../../asset/images/settings.png';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function NavBar() {
+	const [active, setActive] = useState(0);
+	
 	return (
 		<nav>
-			<div>
-				<Link to={"/profile"}>
+			<div className={active === "profile" ? "selected" : ""}>
+				<Link to={"/profile"} onClick={() => setActive("profile")}>
 					<img src={homePageImg} alt="homePage" className="first" />
 				</Link>
 			</div>
 			<div className="line"></div>
-			<div>
-				<Link to={"/message"}>
+			<div className={active === "message" ? "selected" : ""}>
+				<Link to={"/message"} onClick={() => setActive("message")}>
 					<img src={messageImg} alt="" />
 				</Link>
 			</div>
-			<div>
-				<Link to={"/message"}>
+			<div className={active === "user" ? "selected" : ""}>
+				<Link to={"/message"} onClick={() => setActive("message")}>
 					<img src={userImg} alt="" />
 				</Link>
 			</div>
-			<div>
-				<Link to={"/message"}>
+			<div className={active === "friends" ? "selected" : ""}>
+				<Link to={"/message"} onClick={() => setActive("message")}>
 					<img src={crowdImg} alt="" />
 				</Link>
 			</div>
-			<div className="set">
-				<Link to={"/settings"}>
+			<div className={active === "settings" ? "set selected" : "set"}>
+				<Link to={"/settings"} onClick={() => setActive("settings")}>
 					<img src={settings} alt="" />
 				</Link>
 			</div>
