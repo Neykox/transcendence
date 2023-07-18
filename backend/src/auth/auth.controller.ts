cd81fa3ea7ac105f7693941b81379d9278a7b2c0
CAMBIUM camille <ccambium@gmail.com>
2023-07-18 13:28:50 +0200

    Friend-List : making the api call in the back for multi computer
    Friend-List : save

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, UseGuards, Res, } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Res, Param} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Response, Request } from 'express';
 import { UserIdDto } from '../dto/user_id.dto';
@@ -21,5 +21,49 @@ export class AuthController {
 	{
 		response.clearCookie('my_cooky');
 		return {msg: 'cookies cleared'}
+	}
+
+	@Get(':code')
+	async auth(@Param() code) {
+		const get_access_token = async () => {
+			const requestOptions = {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({
+					grant_type: "authorization_code",
+					client_id: process.env.REACT_APP_UID42,
+					client_secret: process.env.REACT_APP_SECRET42,
+					code: code.code,
+					redirect_uri: `http://${process.env.REACT_APP_POSTURL}:3000/page1`
+					// "access_token":code.code
+					// "token_type":"bearer",
+					// "expires_in":7200,
+					// "scope":"public",
+					// "created_at":Date.now()
+				})
+				//body: `grant_type=authorization_code&client_id=${process.env.REACT_APP_UID42}&client_secret=${process.env.REACT_APP_SECRET42}&code=${code.code}&redirect_uri=http%3A%2F%2F10.18.198.79%3A3000%2Fpage1`
+			};
+			const response = await fetch(
+				"https://api.intra.42.fr/oauth/token",
+				requestOptions
+			);
+			const data = await response.json();
+			return data['access_token'];
+		};
+	
+	
+	
+		const get_user_info = async (access_token) => {
+			const requestOptions = {
+				method: "GET",
+				headers: { Authorization: `Bearer ${access_token}` },
+			};
+			const response = await fetch(
+				"https://api.intra.42.fr/v2/me",
+				requestOptions
+			);
+			return response.json();
+		};
+		return (await get_user_info(await get_access_token()));
 	}
 }
