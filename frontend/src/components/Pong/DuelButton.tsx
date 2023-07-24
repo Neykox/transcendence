d675e9b0a923f94955155de7941a57d299417d17
CAMBIUM camille <ccambium@gmail.com>
2023-07-24 12:54:52 +0200

    dual button beatifuller

--- frontend/src/components/Pong/DuelButton.tsx
+++ frontend/src/components/Pong/DuelButton.tsx
@@ -72,7 +72,7 @@ function DuelButton() {
 
 	return (
 		<>
-			<a onClick={() => {setShow(!show)}}><img className="duelButton" src={duel}/></a>
+			<div className='svg'><a onClick={() => {setShow(!show)}}><img className="duelButton" src={duel}/></a></div>
 			{show === true
 			? <>
 				{status === "setting-up"
