6a1d2d6ea900a4e9e695120871c1818a5b7f749a
Neykox <42614911+Neykox@users.noreply.github.com>
2023-08-16 20:23:51 +0200

    seems to be working without ValidationPipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,21 +6,22 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+
+
+
+  ///////////////////////////////////////////////
   // app.useGlobalPipes(new ValidationPipe({
   //   whitelist: true,
   // }));
+  ///////////////////////////////////////////////
+
+
   app.enableCors({
     origin: ["http://localhost:3000"],
     credentials: true,
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
     methods: 'GET, POST, PUT, DELETE, OPTIONS',
   });
-  
-  // app.enableCors({
-  //   origin: '*',
-  //   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
-  //   methods: 'GET, POST, PUT, DELETE, OPTIONS',
-  // });
 
   await app.listen(5000);
 }
