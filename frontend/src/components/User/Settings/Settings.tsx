b239124a4fb316ef14857f8bb4a352b77c4b966d
tanguy <tanguy.hamon@mac.fr>
2023-08-29 05:52:11 +0200

    settings css

--- frontend/src/components/User/Settings/Settings.tsx
+++ frontend/src/components/User/Settings/Settings.tsx
@@ -52,63 +52,63 @@ const Settings = () => {
     localStorage.setItem("user", JSON.stringify(user));
   };
 
-	const handleModalOpen = () => {
-		setIsModalOpen(true);
-	};
+  const handleModalOpen = () => {
+    setIsModalOpen(true);
+  };
 
-	const handleModalClose = () => {
-		setIsModalOpen(false);
-	};
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
 
-	//const handleModalSave = () => {
-	//  user.login = login; 
-	//  setIsModalOpen(false);
-	//};
+  //const handleModalSave = () => {
+  //  user.login = login; 
+  //  setIsModalOpen(false);
+  //};
 
 
-	const handleModalSave = () => {
-		setUser(prevUser => ({ ...prevUser, pseudo }));
-		fetch(`http://${process.env.REACT_APP_POSTURL}:5000/users/changePseudo`, {
-			method: 'PUT',
-			credentials: 'include',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({ Pseudo: pseudo }),
-		})
-			.then(response => {
-				if (response.ok) {
-					toast.success('Pseudo mis à jour avec succès');
-				} else {
-					toast.error('Une erreur est survenue lors de la mise à jour du pseudo');
-				}
-			})
-			.catch(error => {
-				console.error('Erreur lors de la mise à jour du pseudo', error);
-				toast.error('Une erreur est survenue lors de la mise à jour du pseudo');
-			});
-		setIsModalOpen(false);
-	};
+  const handleModalSave = () => {
+    setUser(prevUser => ({ ...prevUser, pseudo }));
+    fetch(`http://${process.env.REACT_APP_POSTURL}:5000/users/changePseudo`, {
+      method: 'PUT',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ Pseudo: pseudo }),
+    })
+      .then(response => {
+        if (response.ok) {
+          toast.success('Pseudo mis à jour avec succès');
+        } else {
+          toast.error('Une erreur est survenue lors de la mise à jour du pseudo');
+        }
+      })
+      .catch(error => {
+        console.error('Erreur lors de la mise à jour du pseudo', error);
+        toast.error('Une erreur est survenue lors de la mise à jour du pseudo');
+      });
+    setIsModalOpen(false);
+  };
 
-	/*
-	evenements de changement de mode 
-	*/
+  /*
+  evenements de changement de mode 
+  */
 
-	const handleDarkModeToggle = () => {
-		setDarkMode(!darkMode);
-	};
+  const handleDarkModeToggle = () => {
+    setDarkMode(!darkMode);
+  };
 
-	/*
-	evenements de changement de son
-	*/
+  /*
+  evenements de changement de son
+  */
 
-	const handleSoundToggle = () => {
-		setSoundEnabled(!soundEnabled);
-	};
+  const handleSoundToggle = () => {
+    setSoundEnabled(!soundEnabled);
+  };
 
-	/* 
-	evenements de changement de double authentification
-	*/
+  /* 
+  evenements de changement de double authentification
+  */
   const doubleAuthhandleSoundToggle = async (e) => {
     setDoubleAuthEnabled(e.target.checked);
     if (e.target.checked === false) {
@@ -125,52 +125,51 @@ const Settings = () => {
       localStorage.setItem("user", await JSON.stringify(user));
       await turnOff();
     }
-    else
-    {
-    	console.log({user})//do a get user idc anymore
+    else {
+      console.log({ user })//do a get user idc anymore
       navigate("/twofa", { state: { signin: false, user: user, image: user.image } });
     }
   };
 
-	/*
-	evenements de changement de photo de profil
-	*/
+  /*
+  evenements de changement de photo de profil
+  */
 
-	const handleProfilePhotoChange = (event: ChangeEvent<HTMLInputElement>) => {
-		if (event.target.files && event.target.files.length > 0) {
-			const file = event.target.files[0];
-			const newAvatarUrl = URL.createObjectURL(file);
-			setProfilePhoto(newAvatarUrl);
-		}
-	};
+  const handleProfilePhotoChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0];
+      const newAvatarUrl = URL.createObjectURL(file);
+      setProfilePhoto(newAvatarUrl);
+    }
+  };
 
-	const handleSave = () => {
+  const handleSave = () => {
 
     if (profilePhoto)
       user.image = profilePhoto;
     else if (selectedDefaultAvatar)
       user.image = selectedDefaultAvatar;
 
-     setUser(prevUser => ({ ...prevUser, Image }));
-     fetch(`http://${process.env.REACT_APP_POSTURL}:5000/users/changeAvatar`, {
-     method: 'PUT',
-     credentials: 'include',
-     headers: {
-       'Content-Type': 'application/json',
-     },
-     body: JSON.stringify({ Image: user.image }),
+    setUser(prevUser => ({ ...prevUser, Image }));
+    fetch(`http://${process.env.REACT_APP_POSTURL}:5000/users/changeAvatar`, {
+      method: 'PUT',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ Image: user.image }),
+    })
+      .then(response => {
+        if (response.ok) {
+          toast.success('Avatar mis à jour avec succès');
+        } else {
+          toast.error('Une erreur est survenue lors de la mise à jour de l\'avatar');
+        }
       })
-     .then(response => {
-       if (response.ok) {
-         toast.success('Avatar mis à jour avec succès');
-       } else {
-         toast.error('Une erreur est survenue lors de la mise à jour de l\'avatar');
-       }
-     })
-     .catch(error => {
-       console.error('Erreur lors de la mise à jour du l\'avatar', error);
-       toast.error('Une erreur est survenue lors de la mise à jour du l\'avatar');
-     });
+      .catch(error => {
+        console.error('Erreur lors de la mise à jour du l\'avatar', error);
+        toast.error('Une erreur est survenue lors de la mise à jour du l\'avatar');
+      });
 
     //if (profilePhoto) {
     //  user.avatar = profilePhoto;
@@ -188,117 +187,122 @@ const Settings = () => {
     setDoubleAuthEnabled(user.is2FaActive);
   }, [user]);
 
-	return (
+  return (
 
 
-		<div>
-			<NavBar />
-			<div className="settinglist">
-				<div>
-					<label htmlFor="login">Modifier son pseudo: </label>
-					<button onClick={handleModalOpen}>Modifier</button>
-
-					{isModalOpen && (
-						<div className="modal">
-							<div className="modal-content">
-								<h2>Modifier le pseudo</h2>
-								<input
-									type="text"
-									id="pseudo"
-									value={pseudo}
-									onChange={handlePseudoChange}
-								/>
-								<div className="modal-buttons">
-									<button onClick={handleModalSave}>Enregistrer</button>
-									<button onClick={handleModalClose}>Annuler</button>
-								</div>
-							</div>
-						</div>
-					)}
-				</div>
-
-				<div>
-					<label htmlFor="darkMode">Activer le dark mode : </label>
-					<Toggle
-						id="darkMode"
-						checked={darkMode}
-						onChange={handleDarkModeToggle}
-					/>
-				</div>
-
-				<div>
-					<label htmlFor="sound">Activer le son : </label>
-					<Toggle
-						id="sound"
-						checked={soundEnabled}
-						onChange={handleSoundToggle}
-					/>
-				</div>
-
-				<div>
-					<label htmlFor="2fa">Activer l'authentification à deux facteurs : </label>
-					<Toggle
-						id="2fa"
-						checked={doubleAuthEnabled}
-						onChange={doubleAuthhandleSoundToggle}
-					/>
-				</div>
-
-
-
-				<div>
-          <label htmlFor="profilePhoto">Modifier sa photo de profil : </label>
-
-          <button onClick={() => setIsModal2Open(true)}>Avatar perso</button>
-          <button onClick={() => setIsDefaultModalOpen(true)}>Avatar par défaut</button>
+    <div>
+      <NavBar />
+      <div className="settinglist">
+        <div className="settingTitle">
+          <h1>Settings</h1>
         </div>
-
-
-
-        <Modal isOpen={isModal2Open}>
-          <h2>Choisir une photo de profil</h2>
-          <input
-            type="file"
-            accept="image/*"
-            onChange={handleProfilePhotoChange}
-          />
-          {profilePhoto && (
-            <div>
-              <h3>Ancienne photo de profil : </h3>
-              <img className="imglist" src={user.image} alt="Avatar" />
-              <h3>Nouvelle photo de profil : </h3>
-              <img className="imglist" src={profilePhoto} alt="Avatar" />
-              <button onClick={handleSave}>Enregistrer</button>
-            </div>
-          )}
-          <button onClick={() => setIsModal2Open(false)}>Fermer</button>
-        </Modal>
-
-        <Modal isOpen={isDefaultModalOpen}>
-          <h2>Choisir un avatar par défaut</h2>
+        <div className="settingsOptions">
           <div>
-            {image42 !== 'null' && (
-              <img className={selectedDefaultAvatar === image42 ? "selectedImage imglist" : "imglist"} src={image42} alt="Avatar" onClick={() => setSelectedDefaultAvatar(() => image42)} />
+            <label htmlFor="login">Modifier son pseudo: </label>
+            <button onClick={handleModalOpen}>Modifier</button>
+
+            {isModalOpen && (
+              <div className="modal">
+                <div className="modal-content">
+                  <h2>Modifier le pseudo</h2>
+                  <input
+                    type="text"
+                    id="pseudo"
+                    value={pseudo}
+                    onChange={handlePseudoChange}
+                  />
+                  <div className="modal-buttons">
+                    <button onClick={handleModalSave}>Enregistrer</button>
+                    <button onClick={handleModalClose}>Annuler</button>
+                  </div>
+                </div>
+              </div>
             )}
-            <img className={selectedDefaultAvatar === test ? "selectedImage imglist" : "imglist"} src={test} alt="Avatar4" onClick={() => setSelectedDefaultAvatar(() => test)} />
-            <img className={selectedDefaultAvatar === arcencielfille ? "selectedImage imglist" : "imglist"} src={arcencielfille} alt="Avatar1" onClick={() => setSelectedDefaultAvatar(() => arcencielfille)} />
-            <img className={selectedDefaultAvatar === chat ? "selectedImage imglist" : "imglist"} src={chat} alt="Avatar2" onClick={() => setSelectedDefaultAvatar(() => chat)} />
-            <img className={selectedDefaultAvatar === fille ? "selectedImage imglist" : "imglist"} src={fille} alt="Avatar3" onClick={() => setSelectedDefaultAvatar(() => fille)} />
-            <img className={selectedDefaultAvatar === lunette2 ? "selectedImage imglist" : "imglist"} src={lunette2} alt="Avatar5" onClick={() => setSelectedDefaultAvatar(() => lunette2)} />
-            <img className={selectedDefaultAvatar === lunette ? "selectedImage imglist" : "imglist"} src={lunette} alt="Avatar6" onClick={() => setSelectedDefaultAvatar(() => lunette)} />
-            <img className={selectedDefaultAvatar === lunettesoleil ? "selectedImage imglist" : "imglist"} src={lunettesoleil} alt="Avatar7" onClick={() => setSelectedDefaultAvatar(() => lunettesoleil)} />
-            <img className={selectedDefaultAvatar === peinture ? "selectedImage imglist" : "imglist"} src={peinture} alt="Avatar8" onClick={() => setSelectedDefaultAvatar(() => peinture)} />
-            <img className={selectedDefaultAvatar === smiley ? "selectedImage imglist" : "imglist"} src={smiley} alt="Avatar9" onClick={() => setSelectedDefaultAvatar(() => smiley)} />
-            <img className={selectedDefaultAvatar === vert ? "selectedImage imglist" : "imglist"} src={vert} alt="Avatar10" onClick={() => setSelectedDefaultAvatar(() => vert)} />
-            <img className={selectedDefaultAvatar === arcencielgarcon ? "selectedImage imglist" : "imglist"} src={arcencielgarcon} alt="Avatar11" onClick={() => setSelectedDefaultAvatar(() => arcencielgarcon)} /> master
           </div>
-          {selectedDefaultAvatar && (
-            <button onClick={handleSave}>Enregistrer</button>
-          )}
-          <button onClick={() => setIsDefaultModalOpen(false)}>Fermer</button>
-        </Modal>
+
+          <div>
+            <label htmlFor="darkMode">Activer le dark mode : </label>
+            <Toggle
+              id="darkMode"
+              checked={darkMode}
+              onChange={handleDarkModeToggle}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="sound">Activer le son : </label>
+            <Toggle
+              id="sound"
+              checked={soundEnabled}
+              onChange={handleSoundToggle}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="2fa">Activer l'authentification à deux facteurs : </label>
+            <Toggle
+              id="2fa"
+              checked={doubleAuthEnabled}
+              onChange={doubleAuthhandleSoundToggle}
+            />
+          </div>
 
 
+
+          <div>
+            <label htmlFor="profilePhoto">Modifier sa photo de profil : </label>
+
+            <button onClick={() => setIsModal2Open(true)}>Avatar perso</button>
+            <button onClick={() => setIsDefaultModalOpen(true)}>Avatar par défaut</button>
+          </div>
+
+
+
+          <Modal isOpen={isModal2Open}>
+            <h2>Choisir une photo de profil</h2>
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleProfilePhotoChange}
+            />
+            {profilePhoto && (
+              <div>
+                <h3>Ancienne photo de profil : </h3>
+                <img className="imglist" src={user.image} alt="Avatar" />
+                <h3>Nouvelle photo de profil : </h3>
+                <img className="imglist" src={profilePhoto} alt="Avatar" />
+                <button onClick={handleSave}>Enregistrer</button>
+              </div>
+            )}
+            <button onClick={() => setIsModal2Open(false)}>Fermer</button>
+          </Modal>
+
+          <Modal isOpen={isDefaultModalOpen}>
+            <h2>Choisir un avatar par défaut</h2>
+            <div>
+              {image42 !== 'null' && (
+                <img className={selectedDefaultAvatar === image42 ? "selectedImage imglist" : "imglist"} src={image42} alt="Avatar" onClick={() => setSelectedDefaultAvatar(() => image42)} />
+              )}
+              <img className={selectedDefaultAvatar === test ? "selectedImage imglist" : "imglist"} src={test} alt="Avatar4" onClick={() => setSelectedDefaultAvatar(() => test)} />
+              <img className={selectedDefaultAvatar === arcencielfille ? "selectedImage imglist" : "imglist"} src={arcencielfille} alt="Avatar1" onClick={() => setSelectedDefaultAvatar(() => arcencielfille)} />
+              <img className={selectedDefaultAvatar === chat ? "selectedImage imglist" : "imglist"} src={chat} alt="Avatar2" onClick={() => setSelectedDefaultAvatar(() => chat)} />
+              <img className={selectedDefaultAvatar === fille ? "selectedImage imglist" : "imglist"} src={fille} alt="Avatar3" onClick={() => setSelectedDefaultAvatar(() => fille)} />
+              <img className={selectedDefaultAvatar === lunette2 ? "selectedImage imglist" : "imglist"} src={lunette2} alt="Avatar5" onClick={() => setSelectedDefaultAvatar(() => lunette2)} />
+              <img className={selectedDefaultAvatar === lunette ? "selectedImage imglist" : "imglist"} src={lunette} alt="Avatar6" onClick={() => setSelectedDefaultAvatar(() => lunette)} />
+              <img className={selectedDefaultAvatar === lunettesoleil ? "selectedImage imglist" : "imglist"} src={lunettesoleil} alt="Avatar7" onClick={() => setSelectedDefaultAvatar(() => lunettesoleil)} />
+              <img className={selectedDefaultAvatar === peinture ? "selectedImage imglist" : "imglist"} src={peinture} alt="Avatar8" onClick={() => setSelectedDefaultAvatar(() => peinture)} />
+              <img className={selectedDefaultAvatar === smiley ? "selectedImage imglist" : "imglist"} src={smiley} alt="Avatar9" onClick={() => setSelectedDefaultAvatar(() => smiley)} />
+              <img className={selectedDefaultAvatar === vert ? "selectedImage imglist" : "imglist"} src={vert} alt="Avatar10" onClick={() => setSelectedDefaultAvatar(() => vert)} />
+              <img className={selectedDefaultAvatar === arcencielgarcon ? "selectedImage imglist" : "imglist"} src={arcencielgarcon} alt="Avatar11" onClick={() => setSelectedDefaultAvatar(() => arcencielgarcon)} />
+            </div>
+            {selectedDefaultAvatar && (
+              <button onClick={handleSave}>Enregistrer</button>
+            )}
+            <button onClick={() => setIsDefaultModalOpen(false)}>Fermer</button>
+          </Modal>
+
+
+        </div>
       </div>
     </div>
   );
